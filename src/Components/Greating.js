a38002040c667163f5716fd26b9693a5a04af1fe
Dipak5487 <dipakchauhan330@gmail.com>
2022-09-25 18:11:58 +0530

    fixed code

--- src/Components/Greating.js
+++ src/Components/Greating.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 const Greating = (props) => {
+    //Register Variable , Method, JavaScript Work Here! 
     const [name, setName] = useState("Learning React App")
     const [input, setInput] = useState("")
 
@@ -16,23 +17,17 @@ const Greating = (props) => {
         setName("Learning React App")
     }
     function handleChanges(e) {
-
         setInput(e.target.value)
-
-
-
-
         console.log("Handle Changes", e)
 
     }
     return (
-        <>
+        <fragment>
             <p>Hello {name}</p>
             <input type="text" name="FirstName" value={input} onChange={handleChanges} />
             <button onClick={changeName}>Change Name</button>
             <button onClick={revert}>Revers Name</button>
-        </>
+        </fragment>
     )
 }
-
 export default Greating
